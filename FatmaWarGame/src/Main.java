45d5b78331e9401f5db957036e1ac001a0976de5
nirme <nirme@10.100.102.105>
2023-03-19 09:28:39 +0200

    few fixes

--- FatmaWarGame/src/Main.java
+++ FatmaWarGame/src/Main.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Arrays;
+
 
 public class Main {
 
 	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		ArrayList<String> mainDeck = createDeck();
         ArrayList<String> playerDeck = new ArrayList<String>();
         ArrayList<String> computerDeck = new ArrayList<String>();
@@ -18,25 +19,23 @@ public class Main {
             computerDeck.add(mainDeck.remove(0));
         }
         
+        System.out.println(Arrays.toString(playerDeck.toArray()));
+        System.out.println(Arrays.toString(computerDeck.toArray()));
+
+        
         System.out.println("Welcome to Fatma War Game!");
         sc.nextLine();
         printNumOfCards(playerDeck, computerDeck);
-        System.out.println("Play?");
-        String userInput = sc.nextLine();
-        if (userInput.toLowerCase().equals("no")) {
-        	System.out.println("AI wins! Good bye");
-        }
-        else if (userInput.equals("")) {
-        	showCards();
-        }
-        else {
-        	System.out.println("The input is wrong. Please try again");
-        }
+        playQuest(playerDeck, computerDeck);
 	}
 
 	public static ArrayList<String> createDeck() {
 		ArrayList<String> deck = new ArrayList<String>();
-		String[] values = {"A","2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+//		String[] values = {"A","2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+		
+		// for warMode debugging
+		String[] values = {"A","A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"};
+
 		
 		// Add 4 cards of each value
 		for (int i = 0; i < 13; i++) {
@@ -61,24 +60,30 @@ public class Main {
 		System.out.println("AI " + playerCurCard);
 		System.out.println("Player " + compCurCard);
 		sc.nextLine();
+		compareCards(playerDeck, computerDeck, playerCurCard, compCurCard);
 		
+	}
+	
+	public static void compareCards(ArrayList<String> playerDeck, ArrayList<String> computerDeck, String playerCurCard, String compCurCard) {
 		// Compare the cards
 	    if (getCardValue(playerCurCard) > getCardValue(compCurCard)) {
 	        // Player wins, add both cards to the end of their deck
 	        playerDeck.add(playerCurCard);
 	        playerDeck.add(compCurCard);
-	        printNumOfCards(playerDeck, computerDeck);
 	        playerDeck.remove(0);
 			computerDeck.remove(0);
+	        printNumOfCards(playerDeck, computerDeck);
+	        
 	    } else if (getCardValue(playerCurCard) < getCardValue(compCurCard)) {
 	        // Computer wins, add both cards to the end of their deck
 	        computerDeck.add(compCurCard);
 	        computerDeck.add(playerCurCard);
-	        printNumOfCards(playerDeck, computerDeck);
 	        playerDeck.remove(0);
 			computerDeck.remove(0);
+	        printNumOfCards(playerDeck, computerDeck);
+	        
 	    } else {
-	        warMode();
+	        warMode(playerDeck, computerDeck);
 	    }
 	}
 	
@@ -114,9 +119,16 @@ public class Main {
 			compWarList.add(computerDeck.get(i));
 		}
 		System.out.print("AI ");
-		for (int i = 0; i < playerWarList.size(); i++) {
-			System.out.print(playerWarList.get(i));
-		}
+		System.out.print(compWarList.get(0));
+		System.out.print("###");
+		System.out.println(compWarList.get(4));
+
+		System.out.print("Player ");
+		System.out.print(playerWarList.get(0));
+		System.out.print("###");
+		System.out.println(playerWarList.get(4));
+		
+		compareCards(playerDeck, computerDeck, playerWarList.get(4), compWarList.get(4));
 	}
 	
 	// A helper method to get the value of a card
@@ -127,19 +139,19 @@ public class Main {
 	    } catch (NumberFormatException e) {
 	        // It's not a number, so it must be a face card or a joker
 	        switch (card) {
-	            case "Joker":
-	                value = 11;
+	            case "Jo":
+	                value = 14;
 	                break;
-	            case "Ace":
+	            case "A":
 	                value = 1;
 	                break;
-	            case "Jack":
+	            case "J":
 	                value = 11;
 	                break;
-	            case "Queen":
+	            case "Q":
 	                value = 12;
 	                break;
-	            case "King":
+	            case "K":
 	                value = 13;
 	                break;
 	        }
